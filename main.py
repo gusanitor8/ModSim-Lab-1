30b70e13f6edb0cd5eceda86a4ec46db780aa1bc
Gustavo Gonzalez <gustavogonzalez@Gustavos-MacBook-Air.local>
2024-07-12 16:51:55 -0600

    functional version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 from dataclasses import dataclass
+import matplotlib.pyplot as plt
 
 PARTICLE_NO = 40
 POSITION_RANGE = (-10, 10)  # we asume the range has a form of an N dimensional cube
@@ -9,7 +10,8 @@ W = 0.5
 C1 = 1.5
 C2 = 1.5
 EPOCHS = 100
-DIMENSIONS = 3
+DIMENSIONS = 2
+DESCENDING = True
 
 
 @dataclass
@@ -17,41 +19,49 @@ class Particle:
     position: np.ndarray
     velocity: float
     best_position: np.ndarray
-    best_value: float
+    best_value: np.float64
 
     def __init__(self):
         self.position = np.array([random.uniform(*POSITION_RANGE) for _ in range(DIMENSIONS)])
         self.velocity = random.uniform(*VELOCITY_RANGE)
         self.best_position = self.position
-        self.best_value = function(self.position, self.position)
+        self.best_value = function_(self.position)
 
 
 def main():
     random.seed(0)
-    velocity, particles, global_best = init_vars()
+    velocity, particles, global_best_value = init_vars()
 
     for _ in range(EPOCHS):
         # TODO: revisar esto
         for particle in particles:
-            particle.velocity = get_next_velocity(velocity, global_best, particle)
+            particle.velocity = get_next_velocity(velocity, global_best_value, particle)
             particle.position = next_position(particle.position, particle.velocity)
-            value = function(*particle.position)
-            if value < particle.best_value:
-                particle.best_value = value
-                particle.best_position = particle.position
-            if value < global_best.best_value:
-                global_best = particle
+            position_value_z = function_(particle.position)
 
+            if (DESCENDING and position_value_z < particle.best_value) or (
+                    not DESCENDING and position_value_z > particle.best_value):
+                particle.best_position = particle.position
+                particle.best_value = position_value_z
+
+            if (DESCENDING and position_value_z < global_best_value) or (
+                    not DESCENDING and position_value_z > global_best_value):
+                global_best_value = position_value_z
+
+    print(global_best_value)
+    print_xy(particles)
 
 def init_vars():
     velocity = random.uniform(-1, 1)
     particles = [Particle() for _ in range(PARTICLE_NO)]
-    global_best = max(particles, key=lambda p: p.best_position[-1])
-    return velocity, particles, global_best
+    global_best = max(particles, key=lambda p: function_(p.best_position))
+    return velocity, particles, global_best.best_value
 
 
-def function(x, y):
-    return (x - 3) ** 2 + (y - 2) ** 2
+def function_(vector: np.ndarray) -> np.float64:
+    x = vector[0]
+    y = vector[1]
+    return np.float64((x - 3) ** 2 + (y - 2) ** 2)
 
 
 def get_next_velocity(velocity, global_best, particle: Particle):
@@ -64,6 +74,25 @@ def get_next_velocity(velocity, global_best, particle: Particle):
 def next_position(position, velocity):
     return position + velocity
 
+
+def print_xy(particles):
+    particle_pos = []
+    for particle in particles:
+        particle_pos.append(particle.position)
+
+    # Unzip the list of tuples into two lists: x and y
+    x, y = zip(*particle_pos)
+
+    # Create the scatterplot
+    plt.scatter(x, y)
+
+    # Add labels and title
+    plt.xlabel('X-axis label')
+    plt.ylabel('Y-axis label')
+    plt.title('Scatterplot Title')
+
+    # Show the plot
+    plt.show()
 
 if __name__ == "__main__":
     main()
